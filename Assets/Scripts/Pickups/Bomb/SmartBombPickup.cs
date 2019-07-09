1eb7f9db406fa35ca3d12b5a4d61f635717f70dd
Bradley Katz <brad.katz13@gmail.com>
2019-07-09 16:33:37 -0400

    Finished SmartBomb Pickup, Implemented LaserPickup

--- Assets/Scripts/Pickups/Bomb/SmartBombPickup.cs
+++ Assets/Scripts/Pickups/Bomb/SmartBombPickup.cs
@@ -18,6 +18,6 @@ public class SmartBombPickup : PickupBase
 
     protected override void OnPickupGrabbedEffect(PlayerManager player)
     {
-
+        player.IncrementBombs();
     }
 }
--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 [RequireComponent(typeof(PlayerMovement))]
 [RequireComponent(typeof(PlayerHealth))]
+[RequireComponent(typeof(PlayerShoot))]
 public class PlayerManager : MonoBehaviour
 {
     protected PlayerMovement playerMovement = null;
     protected PlayerHealth playerHealth = null;
+    protected PlayerShoot playerShoot = null;
 
     protected void Awake()
     {
         playerMovement = GetComponent<PlayerMovement>();
         playerHealth = GetComponent<PlayerHealth>();
+        playerShoot = GetComponent<PlayerShoot>();
     }
 
     public void Heal(float healAmount)
@@ -23,5 +26,15 @@ public class PlayerManager : MonoBehaviour
     public void IncrementGoldRings()
     {
         playerHealth.IncrementGoldRings();
+    }
+
+    public void IncrementBombs()
+    {
+        playerShoot.IncrementBombs();
+    }
+
+    public void UpdateLaserType()
+    {
+        playerShoot.UpdateLaserType();
     }
 }
