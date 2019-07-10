fa16d1e449ec81b440290660512e187b630310ce
Bradley Katz <brad.katz13@gmail.com>
2019-07-09 23:01:08 -0400

    Lasers can now damage other game entities

---
+++ Assets/Scripts/Health/Damageable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    [Range(1.0f, 50.0f)]
+    public float maxHealth;
+    protected float currentHealth;
+
+    protected void Start()
+    {
+        currentHealth = maxHealth;   
+    }
+
+    public void OnDamageTaken(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0.0f)
+        {
+            OnDeath();
+        }
+    }
+
+    protected virtual void OnDeath()
+    {
+        Destroy(this.gameObject);
+    }
+}
